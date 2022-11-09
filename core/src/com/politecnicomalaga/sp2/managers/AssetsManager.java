8093e41417ad8c28b67cfb245c3cb272cd741dae
DaviMontiel <davidmontielnieto5@gmail.com>
2022-11-09 11:23:19 +0100

    SI2_v0.2.2: AssetsManager, SettingManager, GameManager, assets/, actualizados

--- core/src/com/politecnicomalaga/sp2/managers/AssetsManager.java
+++ core/src/com/politecnicomalaga/sp2/managers/AssetsManager.java
@@ -1,8 +1,33 @@
 package com.politecnicomalaga.sp2.managers;
 
+///////////////
+// CONSTANTS //
+///////////////
+
 public class AssetsManager {
-    //Constantes
-    public static final String NUMBERS_SPRITES = "numbers/digito";
+
+    // SPRITE
+    public static final String NUMBERS_SPRITES ="numbers/digito";
     public static final String NUMBERS_EXT_SPRITES =".png";
+
+    // ATLAS
+    public static final String ATLAS_PATH ="assets/sp2.png";
+    public static final String ATLAS_CUSTOM_PATH ="assets/sp2_Custom.png";
+    public static final String REGION_PLAYER_NAME ="player";
+    public static final String REGION_ENEMY_NAME ="ovni";
+    public static final String REGION_EXPLOSION_NAME ="explosion";
+
+    // SOUND
+    public static final String SOUND_PATH ="assets/sounds/";
+    public static final String SOUND_DAMAGE ="damage.wav";
+    public static final String SOUND_DAMAGE1 ="damage1.wav";
+    public static final String OST_GAME_MUSIC ="gamemusic.ogg";
+    public static final String OST_INTO ="gamemusic.ogg";
+    public static final String SOUND_MOVE ="move.wav";
+
+    // DIGITS
+    public static final String DIGIT_PATH ="assets/numbers/";
+    public static final String DIGIT_NAME ="digito";
+    public static final String DIGIT_EXT =".png";
 
 }//CLASS
--- core/src/com/politecnicomalaga/sp2/managers/SettingsManager.java
+++ core/src/com/politecnicomalaga/sp2/managers/SettingsManager.java
@@ -1,8 +1,27 @@
 package com.politecnicomalaga.sp2.managers;
 
+///////////////
+// CONSTANTS //
+///////////////
+
 public class SettingsManager {
 
-    // Constants
-    public static final short SCREEN_WIDTH = 1024;
-    public static final short SCREEN_HEIGHT = 768;
-}
+    // WINDOW
+    public static final short SCREEN_MID_WIDTH = 400;
+    public static final short SCREEN_MID_HEIGHT = 300;
+    public static final short SCREEN_WIDTH = SCREEN_MID_WIDTH*2;
+    public static final short SCREEN_HEIGHT = SCREEN_MID_HEIGHT*2;
+
+    // ACTOR
+    public static final short PLAYER_SIZE = 50;
+    public static final short ENEMY_SIZE = 50;
+
+    // SQUAD
+    public static final short BATTALION_SIZE = 4;
+    public static final short SQUAD_SIZE = 6;
+
+    // ANIMATION
+    public static final float ANIMATION_TIME = 0.25f;
+
+
+}//CLASS
