ff042231996d4966bd477ee2bef393221f7b100d
DaviMontiel <davidmontielnieto5@gmail.com>
2022-11-09 12:03:38 +0100

    SI2_v0.2.3: AssetsManager actualizado. sp2_Custom.atlas aniadido

--- core/src/com/politecnicomalaga/sp2/managers/AssetsManager.java
+++ core/src/com/politecnicomalaga/sp2/managers/AssetsManager.java
@@ -11,14 +11,14 @@ public class AssetsManager {
     public static final String NUMBERS_EXT_SPRITES =".png";
 
     // ATLAS
-    public static final String ATLAS_PATH ="assets/sp2.png";
-    public static final String ATLAS_CUSTOM_PATH ="assets/sp2_Custom.png";
+    public static final String ATLAS_PATH ="sp2.atlas";
+    public static final String ATLAS_CUSTOM_PATH ="sp2_Custom.atlas";
     public static final String REGION_PLAYER_NAME ="player";
     public static final String REGION_ENEMY_NAME ="ovni";
     public static final String REGION_EXPLOSION_NAME ="explosion";
 
     // SOUND
-    public static final String SOUND_PATH ="assets/sounds/";
+    public static final String SOUND_PATH ="sounds/";
     public static final String SOUND_DAMAGE ="damage.wav";
     public static final String SOUND_DAMAGE1 ="damage1.wav";
     public static final String OST_GAME_MUSIC ="gamemusic.ogg";
@@ -26,7 +26,7 @@ public class AssetsManager {
     public static final String SOUND_MOVE ="move.wav";
 
     // DIGITS
-    public static final String DIGIT_PATH ="assets/numbers/";
+    public static final String DIGIT_PATH ="numbers/";
     public static final String DIGIT_NAME ="digito";
     public static final String DIGIT_EXT =".png";
 
