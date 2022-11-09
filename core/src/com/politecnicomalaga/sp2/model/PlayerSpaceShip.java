f193545571e4535fbe6ed1dd95a10e1c30a7374e
JuanMaciasGallardo <juanmaciasgallardo@gmail.com>
2022-11-09 11:08:48 +0100

    SI2 V0.2.1 - Cambios al jugador

--- core/src/com/politecnicomalaga/sp2/model/PlayerSpaceShip.java
+++ core/src/com/politecnicomalaga/sp2/model/PlayerSpaceShip.java
@@ -7,21 +7,23 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.politecnicomalaga.sp2.managers.GameManager;
+import com.politecnicomalaga.sp2.managers.SettingsManager;
 
 public class PlayerSpaceShip extends Actor {
-
     private Animation<TextureRegion> skin;
 
     public PlayerSpaceShip() {
         super();
-        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("images/player.pack"));
-        skin = new Animation<TextureRegion>(0.25f, atlas.findRegions("blue"), Animation.PlayMode.LOOP);
-        // Añade setBounds, setX y setY
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("sp2.atlas"));
+        skin = new Animation<TextureRegion>(0.25f, atlas.findRegions("player"), Animation.PlayMode.LOOP);
+        this.setBounds(0, 0, 50,  50);
+        this.setX(0);
+        this.setY(0);
     }
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
         TextureRegion currentFrame = skin.getKeyFrame(GameManager.getSingleton().getGameTime(), true);
-        batch.draw(currentFrame, this.getX(), this.getY());
+        batch.draw(currentFrame, this.getX(), this.getY(), 50,  50);
     }
 }
