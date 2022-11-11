f1d39d0f2b301e57746a66ea16eb15aec8469759
Mipguerrero26 <mipguerrero26@gmail.com>
2022-11-11 10:51:39 +0100

    SI2 V0.5 - migracion de menemy - V0.2.2

--- core/src/com/politecnicomalaga/sp2/model/Squadron.java
+++ core/src/com/politecnicomalaga/sp2/model/Squadron.java
@@ -1,21 +1,34 @@
 package com.politecnicomalaga.sp2.model;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 
 public class Squadron {
 
     private Array<EnemyShip> troops;
+    EnemyShip newEnemy;
+    private int numTroops = 4;
 
-    public Squadron(Stage baseStage) {
+    public Squadron(Stage baseStage, float posX, float posY) {
         //Initiate the arraylist
         troops = new Array<EnemyShip>();
 
-
         //We have to create all the squadrons
-        for (int i=0;i<4;i++) {
-            EnemyShip newEnemy = new EnemyShip();
+        for (int i = 0; i < numTroops; i++) {
+            newEnemy = new EnemyShip((((posX / (numTroops + 1)) * (i + 1)) - 25), posY);
             baseStage.addActor(newEnemy);
+            troops.add(newEnemy);
+
         }
+
     }
+
+    public void draw(Batch batch, float parentAlpha){
+        for (EnemyShip enemys :troops) {
+            enemys.draw(batch, parentAlpha);
+        }
+
+    }
+
 }
